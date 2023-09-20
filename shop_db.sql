ebaf8d4beab7e5cedd40bd1238fd89b9fc0ed4a3
Srishti Shetty <srishtishetty30@gmail.com>
2023-09-20 21:59:29 +0530

    hjg

--- shop_db.sql
+++ shop_db.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3308
--- Generation Time: Sep 20, 2023 at 07:35 AM
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2023 at 02:57 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,16 +30,40 @@ SET time_zone = "+00:00";
 CREATE TABLE `admins` (
   `id` int(100) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `phone_no` int(10) NOT NULL,
+  `email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
 --
 
-INSERT INTO `admins` (`id`, `name`, `password`) VALUES
-(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
-(2, 'kim', '3055effa054a24f84cf42cea946db4cdf445cb76');
+INSERT INTO `admins` (`id`, `name`, `password`, `phone_no`, `email`) VALUES
+(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 0, ''),
+(2, 'kim', '3055effa054a24f84cf42cea946db4cdf445cb76', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_profile`
+--
+
+CREATE TABLE `business_profile` (
+  `bid` int(11) NOT NULL,
+  `adminid` int(100) NOT NULL,
+  `fname` varchar(15) NOT NULL,
+  `lname` varchar(15) NOT NULL,
+  `companyname` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `pincode` int(6) NOT NULL,
+  `ieccode` char(10) NOT NULL,
+  `adcode` char(14) NOT NULL,
+  `gstin` char(15) NOT NULL,
+  `lutcode` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -76,7 +100,7 @@ CREATE TABLE `messages` (
   `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `number` varchar(12) NOT NULL,
+  `phone_no` int(10) NOT NULL,
   `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -84,8 +108,8 @@ CREATE TABLE `messages` (
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
-(1, 1, 'sheik', 'sheik@gmail.com', '12345', 'hi,wassup?\r\n\r\n');
+INSERT INTO `messages` (`id`, `user_id`, `name`, `email`, `phone_no`, `message`) VALUES
+(1, 1, 'sheik', 'sheik@gmail.com', 12345, 'hi,wassup?\r\n\r\n');
 
 -- --------------------------------------------------------
 
@@ -96,25 +120,23 @@ INSERT INTO `messages` (`id`, `user_id`, `name`, `email`, `number`, `message`) V
 CREATE TABLE `orders` (
   `id` int(100) NOT NULL,
   `user_id` int(100) NOT NULL,
+  `pid` int(100) NOT NULL,
+  `pname` varchar(30) NOT NULL,
   `name` varchar(20) NOT NULL,
   `number` varchar(10) NOT NULL,
   `email` varchar(50) NOT NULL,
   `method` varchar(50) NOT NULL,
   `address` varchar(500) NOT NULL,
-  `total_products` varchar(1000) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `country` varchar(30) NOT NULL,
+  `pincode` int(6) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `delivery_charge` decimal(10,2) NOT NULL,
   `total_price` int(100) NOT NULL,
   `placed_on` date NOT NULL DEFAULT current_timestamp(),
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
-(1, 1, 'sheik', '12345', 'sheik@gmail.com', 'cash on delivery', 'flat no. 001, nagasandra, bangalore, karnataka, India - 560008', 'Fridge (70000 x 1) - ', 70000, '2023-08-01', 'completed'),
-(2, 2, 'a', '21', 'a@gmail.com', 'cash on delivery', 'flat no. aaa, aaa, a, a, aa - 2', 'Fridge (70000 x 1) - ', 70000, '2023-08-04', 'pending'),
-(3, 4, 's', '22', 's@gmail.com', 'cash on delivery', 'flat no. a, a, aa, a, aa - 123', 'Dell Inspiron Laptop (120000 x 2) - ', 240000, '2023-08-04', 'pending');
 
 -- --------------------------------------------------------
 
@@ -123,9 +145,10 @@ INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `add
 --
 
 CREATE TABLE `products` (
-  `id` int(100) NOT NULL,
+  `pid` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `details` varchar(500) NOT NULL,
+  `weight` int(11) NOT NULL,
   `price` int(10) NOT NULL,
   `image_01` varchar(100) NOT NULL,
   `image_02` varchar(100) NOT NULL,
@@ -136,12 +159,40 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `details`, `price`, `image_01`, `image_02`, `image_03`) VALUES
-(1, 'Fridge', 'hello', 70000, 'fridge-1.webp', 'fridge-2.webp', 'fridge-3.webp'),
-(2, 'watch', 'AP ref:15407', 12875060, 'watch 1.jpeg', 'watch2.jpeg', 'watch3.jpeg'),
-(3, 'Samsung TV', 'Samsung qn900c ', 1224000, 'tv2.jpeg', 'tv3.jpeg', 'tv1.jpeg'),
-(4, 'Dell Inspiron Laptop', 'Dell Inspiron 2023', 120000, 'laptop1.jpeg', 'laptop2.jpeg', 'laptop3.jpeg'),
-(5, 'iphone 12 pro max smartphone', 'iphone 12 pro max ', 119000, 'iphone1.jpeg', 'iphone2.jpeg', 'iphone3.jpg');
+INSERT INTO `products` (`pid`, `name`, `details`, `weight`, `price`, `image_01`, `image_02`, `image_03`) VALUES
+(1, 'Fridge', 'hello', 0, 70000, 'fridge-1.webp', 'fridge-2.webp', 'fridge-3.webp'),
+(2, 'watch', 'AP ref:15407', 0, 12875060, 'watch 1.jpeg', 'watch2.jpeg', 'watch3.jpeg'),
+(3, 'Samsung TV', 'Samsung qn900c ', 0, 1224000, 'tv2.jpeg', 'tv3.jpeg', 'tv1.jpeg'),
+(4, 'Dell Inspiron Laptop', 'Dell Inspiron 2023', 0, 120000, 'laptop1.jpeg', 'laptop2.jpeg', 'laptop3.jpeg'),
+(5, 'iphone 12 pro max smartphone', 'iphone 12 pro max ', 0, 119000, 'iphone1.jpeg', 'iphone2.jpeg', 'iphone3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tariff`
+--
+
+CREATE TABLE `tariff` (
+  `tid` int(11) NOT NULL,
+  `country` varchar(30) NOT NULL,
+  `tariff` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tariff`
+--
+
+INSERT INTO `tariff` (`tid`, `country`, `tariff`) VALUES
+(1, 'Australia', 0.86),
+(2, 'Canada', 1.47),
+(3, 'China', 3.39),
+(4, 'France', 1.69),
+(5, 'Ireland', 1.69),
+(6, 'Japan', 2.45),
+(7, 'New Zealand', 1.35),
+(8, 'UAE', 4.40),
+(9, 'USA', 1.59),
+(10, 'UK', 1.69);
 
 -- --------------------------------------------------------
 
@@ -200,28 +251,48 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `business_profile`
+--
+ALTER TABLE `business_profile`
+  ADD PRIMARY KEY (`bid`),
+  ADD UNIQUE KEY `ieccode` (`ieccode`),
+  ADD UNIQUE KEY `gstin` (`gstin`),
+  ADD KEY `adminid` (`adminid`);
+
+--
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pid` (`pid`);
 
 --
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pid` (`pid`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `tariff`
+--
+ALTER TABLE `tariff`
+  ADD PRIMARY KEY (`tid`);
 
 --
 -- Indexes for table `users`
@@ -246,6 +317,12 @@ ALTER TABLE `admins`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `business_profile`
+--
+ALTER TABLE `business_profile`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
@@ -267,7 +344,13 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `pid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tariff`
+--
+ALTER TABLE `tariff`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -280,6 +363,36 @@ ALTER TABLE `users`
 --
 ALTER TABLE `wishlist`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `business_profile`
+--
+ALTER TABLE `business_profile`
+  ADD CONSTRAINT `business_profile_ibfk_1` FOREIGN KEY (`adminid`) REFERENCES `admins` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `products` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `products` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
